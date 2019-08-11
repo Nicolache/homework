7be46dfc536a5ce3a3d734a72f1450b1588cb6f7
nik <soundriver@mail.ru>
2019-08-11 22:06:27 +0300

    Functions were made uneversal for any part of speech, and any code tree node.

--- command_line_arguments.py
+++ command_line_arguments.py
@@ -16,3 +16,32 @@ parser.add_argument(
     action='store_true',
     help='It clones all from repos_to_clone_urls.',
 )
+parser.add_argument(
+    '-r',
+    '--report_format',
+    choices=['console', 'json', 'csv'],
+    default='console',
+    help="A report format choise.",
+)
+parser.add_argument(
+    '-o',
+    '--output',
+    action='store',
+    type=str,
+    help="Redirect output to a file.",
+)
+parser.add_argument(
+    '-s',
+    '--search_in',
+    choices=['functions', 'variables'],
+    default='functions',
+    help="Search parts of speech in functions names, or variables names. Functions is default.",
+)
+parser.add_argument(
+    '-p',
+    '--part',
+    choices=['verbs', 'nouns'],
+    default='verbs',
+    help="A part of speech. A choice between nouns, and verbs statistics. Verbs is default.",
+)
+args = parser.parse_args()
--- parts_of_speech_statistics.py
+++ parts_of_speech_statistics.py
@@ -1,12 +1,12 @@
 import ast
 import collections
-# import logging
 import os
 
 from nltk import pos_tag
 from vcstools import get_vcs_client
 
 from command_line_arguments import *
+from output_format import *
 from variables import *
 
 
@@ -25,6 +25,10 @@ def clone_all():
     for url_and_vcstype in repos_to_clone_urls:
         repo_clone(url_and_vcstype[0], url_and_vcstype[1])
 
+def projects_list():
+    for directory in os.listdir(repos_local_path):
+        yield os.path.join(repos_local_path, directory)
+
 
 def flat(folded_generator_with_verbs):
     """Generating verbs with folded generators
@@ -40,18 +44,13 @@ def flat(folded_generator_with_verbs):
             yield folded_item
 
 
-def is_verb(word):
-    """Check if a string is a verb.
-
-    Keyword arguments:
-    word -- A string that is checked.
-
-    The return type is `bool`.
-    """
+def word_belongs_to_parts_of_speech(word, abbreviations):
+    logging.debug('word_belongs ' + word)
+    logging.debug('abbreviations ' + str(abbreviations))
     if not word:
         return False
     pos_info = pos_tag([word])
-    return pos_info[0][1] == 'VB'
+    return pos_info[0][1] in abbreviations
 
 
 def get_filenames():
@@ -102,16 +101,17 @@ def get_trees(with_filenames=False, with_file_content=False):
     logging.info('trees generated')
 
 
-def get_verbs_from_function_name(function_name):
-    """Split function name into words and return verbs.
+def get_poss_from_name(name, abbreviations):
+    """Splits name into words and returns a selected parts of speech.
 
     Keyword arguments:
-    function_name -- A string that contains a function name.
+    name -- A string that contains a function name, or a variable name, or etc. .
+    abbreviations -- Codes of parts of speech in their different forms.
 
     Returns a generator.
     """
-    for word in function_name.split('_'):
-        if is_verb(word):
+    for word in name.split('_'):
+        if word_belongs_to_parts_of_speech(word, abbreviations):
             yield word
 
 
@@ -126,6 +126,34 @@ def generate_nodes_out_of_trees(trees):
     for tree in trees:
         for node in ast.walk(tree):
             yield node
+
+
+def select_names_from_nodes(nodes, search_in):
+    """Returns all function, or variable names from all the nodes.
+
+    Keyword arguments:
+    nodes -- all the units in programming code tree.
+
+    Returns a generator of function, or variable names from all the nodes.
+    """
+    if search_in == 'functions':
+        names = select_function_names_from_nodes(nodes)
+    if search_in == 'variables':
+        names = select_variable_names_from_nodes(nodes)
+    return names
+
+
+def select_variable_names_from_nodes(nodes):
+    """Extracts from nodes all the variables names in lowercase.
+
+    Keyword arguments:
+    nodes -- Nodes of some computer language code.
+
+    Returns a generator.
+    """
+    for node in ast.walk(tree):
+        if isinstance(node, ast.Name):
+            yield node.id.lower()
 
 
 def select_function_names_from_nodes(nodes):
@@ -143,17 +171,20 @@ def select_function_names_from_nodes(nodes):
                 yield node.name.lower()
 
 
-def select_verbs_from_function_names(function_names_in_lower_case):
-    """Generates verbs out of function names plenty.
+def select_pos_from_names(names_in_lower_case, abbreviations):
+    """Generates parts of speech out of functions, variables, etc. names plenty.
+    pos is part of speech.
 
     Keyword arguments:
-    path -- A project path string.
-    top_size -- Limiting the max number of words.
+    names_in_lower_case -- Names of variables, or functions, or etc. in a programming code.
+    abbreviations -- Codes of parts of speech in their different forms.
 
     Returns a generator.
     """
-    for function_name in function_names_in_lower_case:    
-        yield get_verbs_from_function_name(function_name)
+    logging.debug('select_pos_from_names: arg:names_in_lower_case - generator select_function_names_from_nodes')
+    parts_of_speech = []
+    for name in names_in_lower_case:    
+        yield get_poss_from_name(name, abbreviations)
 
 
 def get_top_verbs_in_path(path, top_size=10):
@@ -168,15 +199,14 @@ def get_top_verbs_in_path(path, top_size=10):
     global Path
     Path = path
     nodes = generate_nodes_out_of_trees(get_trees())
-    function_names_in_lower_case = select_function_names_from_nodes(nodes)
-    logging.info('functions extracted')
-    lists_of_verbs = select_verbs_from_function_names(function_names_in_lower_case)
-    verbs = flat(lists_of_verbs)
-    return collections.Counter(verbs).most_common(top_size)
+    names_in_lower_case = select_names_from_nodes(nodes, args.search_in)
+    logging.info('Names extracted.')
+    parts_of_speech = select_pos_from_names(names_in_lower_case, abbreviation_sets[args.part])
+    unfolded_parts_of_speech = flat(parts_of_speech)
+    return collections.Counter(unfolded_parts_of_speech).most_common(top_size)
 
 
 def main():
-    args = parser.parse_args()
     logging.debug(parser)
     logging.debug(args)
 
@@ -185,6 +215,13 @@ def main():
     
     if args.clone:
         clone_all()
+
+    projects = projects_list()
+    words = []
+    for path in projects:
+        words += get_top_verbs_in_path(path)
+
+    console_json_csv_output(words)
 
 
 if __name__ == "__main__":
