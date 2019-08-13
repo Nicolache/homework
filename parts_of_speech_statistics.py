c0518c1e6fb2503bc55301da84004d3da1fe824c
Nick Snegin <soundriver@mail.ru>
2019-08-13 16:57:34 +0300

    pyhon_parsing.py created.

--- parts_of_speech_statistics.py
+++ parts_of_speech_statistics.py
@@ -7,6 +7,7 @@ from vcstools import get_vcs_client
 
 from command_line_arguments import *
 from output_format import *
+from python_parsing import get_trees
 from variables import *
 
 
@@ -53,55 +54,55 @@ def word_belongs_to_parts_of_speech(word, abbreviations):
     return pos_info[0][1] in abbreviations
 
 
-def get_filenames(path):
-    """Get all *.py files locations inside `path` location.
+# def get_filenames(path):
+#     """Get all *.py files locations inside `path` location.
+#
+#     path -- A path string.
+#
+#     Returns a generator.
+#     """
+#     filenames_counter = 0
+#     for dirname, dirs, files in os.walk(path, topdown=True):
+#         for file in files:
+#             if file.endswith('.py') and filenames_counter < MAXFILENAMES:
+#                 filenames_counter += 1
+#                 yield os.path.join(dirname, file)
 
-    path -- A path string.
 
-    Returns a generator.
-    """
-    filenames_counter = 0
-    for dirname, dirs, files in os.walk(path, topdown=True):
-        for file in files:
-            if file.endswith('.py') and filenames_counter < MAXFILENAMES:
-                filenames_counter += 1
-                yield os.path.join(dirname, file)
-
-
-def get_trees(path, with_filenames=False, with_file_content=False):
-    """Generates ast objects, or ast objects in tuple
-    with filenames, and file contents.
-
-    Keyword arguments:
-    path -- A path string.
-    with_filenames -- `bool`:
-        A flag that switches "with filenames" return mode on:
-        ((filename, tree), ...)
-    with_file_content -- `bool`:
-        A flag that switches "with file content" return mode on:
-        ((filename, main_file_content, tree), ...)
-
-    Returns a generator.
-    """
-    filenames = get_filenames(path)
-    filenames_counter = 0
-    for filename in filenames:
-        with open(filename, 'r', encoding='utf-8') as attempt_handler:
-            main_file_content = attempt_handler.read()
-        try:
-            tree = ast.parse(main_file_content)
-            filenames_counter += 1
-        except SyntaxError as e:
-            logging.warning(e)
-            tree = None
-        if not with_filenames:
-            yield tree
-        if with_filenames and not with_filenames:
-            yield (filename, tree)
-        if with_filenames and with_file_content:
-            yield (filename, main_file_content, tree)
-    logging.info('Total {} files'.format(filenames_counter))
-    logging.info('trees generated')
+# def get_trees(path, with_filenames=False, with_file_content=False):
+#     """Generates ast objects, or ast objects in tuple
+#     with filenames, and file contents.
+#
+#     Keyword arguments:
+#     path -- A path string.
+#     with_filenames -- `bool`:
+#         A flag that switches "with filenames" return mode on:
+#         ((filename, tree), ...)
+#     with_file_content -- `bool`:
+#         A flag that switches "with file content" return mode on:
+#         ((filename, main_file_content, tree), ...)
+#
+#     Returns a generator.
+#     """
+#     filenames = get_filenames(path)
+#     filenames_counter = 0
+#     for filename in filenames:
+#         with open(filename, 'r', encoding='utf-8') as attempt_handler:
+#             main_file_content = attempt_handler.read()
+#         try:
+#             tree = ast.parse(main_file_content)
+#             filenames_counter += 1
+#         except SyntaxError as e:
+#             logging.warning(e)
+#             tree = None
+#         if not with_filenames:
+#             yield tree
+#         if with_filenames and not with_filenames:
+#             yield (filename, tree)
+#         if with_filenames and with_file_content:
+#             yield (filename, main_file_content, tree)
+#     logging.info('Total {} files'.format(filenames_counter))
+#     logging.info('trees generated')
 
 
 def get_poss_from_name(name, abbreviations):
--- tasklist.md
+++ tasklist.md
@@ -1,2 +1,5 @@
 ## A tasklist to 2nd homework. ##
 - [ ] To try to choose one and the same strings formatting.
+- [ ] Describe in readme, what is the main *.py file.
+- [ ] Check pep8
+- [ ] Replace * with functions names in imports
